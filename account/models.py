7c8fc3a2b726d09b61619ebe7aea66e6e0799eb9
mdshofiullah <mdshofiullah65@gmail.com>
2022-09-11 02:59:09 +0600

    authentication customization start

--- account/models.py
+++ account/models.py
@@ -4,6 +4,44 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from django.contrib.auth.models import User
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+
+
+class CustomManager(BaseUserManager):
+    def create_user(self, email, user_name, password, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, user=user_name, **extra_fields)
+        user.set_passworde(make_password(password))
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, user_name, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_varify', True)
+        extra_fields.setdefault('user_type', 'developer')
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('superuser must be is_staff=true')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('superuser must be is_superuser=true')
+
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('superuser must be is_active=true')
+
+        if extra_fields.get('is_varify') is not True:
+            raise ValueError('superuser must be is_varify=true')
+
+        if extra_fields.get('user_type') is not True:
+            raise ValueError('superuser must be user_type=true')
+
+        return self.create_user(email, user_name, password, **extra_fields)
 
 
 class Profile(models.Model):
